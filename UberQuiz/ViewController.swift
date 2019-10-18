27f7f3419f3669b731acefec783e792e6f77d4fc
Yumenosuke Kokata <y-kokata@r-up.jp>
2019-10-18 17:17:39 +0900

    Initial Commit

---
+++ UberQuiz/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  UberQuiz
+//
+//  Created by yumenosuke koukata on 2019/10/18.
+//  Copyright © 2019 yumenosuke koukata. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
